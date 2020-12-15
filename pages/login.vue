5c88f7e185cd4d78656c87872acbc0680c672460
Meet Joshi <meetjoshiraru@gmail.com>
2020-12-15 11:34:39 +0530

    login page created

---
+++ pages/login.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="shadow-lg p-3 mb-5 bg-white rounded login-panel text-center">
+    <div class="h5">
+      Login
+    </div>
+    <b-form @submit="onSubmit">
+      <b-form-group
+        id="input-group-1"
+        label="Email address:"
+        label-for="input-1"
+      >
+        <b-form-input
+          id="input-1"
+          v-model="form.email"
+          type="email"
+          placeholder="Enter email"
+          required
+        />
+      </b-form-group>
+      <b-form-group id="input-group-2" label="Passwors" label-for="input-2">
+        <b-form-input
+          id="input-2"
+          v-model="form.password"
+          type="password"
+          placeholder="Enter Password"
+          required
+        />
+      </b-form-group>
+      <b-button type="submit" variant="primary">
+        Submit
+      </b-button>
+    </b-form>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      form: {
+
+        email: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    onSubmit (event) {
+      event.preventDefault()
+      alert(JSON.stringify(this.form))
+    }
+  }
+}
+</script>
+
+<style>
+.login-panel{
+   max-width: 50vw;
+}
+</style>
